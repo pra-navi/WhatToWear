6860a50c624e30215696d7172b932cc2fe2862ae
KumChaiYin <kumchaiyin@gmail.com>
2024-01-21 01:58:02 +0800

    correct getAll reducer

--- client/src/reducers/wardrobe.js
+++ client/src/reducers/wardrobe.js
@@ -13,16 +13,14 @@ const wardrobeReducer = (state = { tops: [], bottoms: [], fullOutfits: [] }, act
             return { ...state, fullOutfits: [...state.fullOutfits, action.payload]};
         
         case GET_TOP: 
-            console.log([...state.tops, action.payload]);
-            return { ...state, tops: [...state.tops, action.payload]};
+            // console.log([...state.tops, action.payload]);
+            return { ...state, tops: action.payload};
         
         case GET_BOTTOM: 
-            console.log([...state.bottoms, action.payload]);
-            return { ...state, bottoms: [...state.bottoms, action.payload]};
+            return { ...state, bottoms: action.payload};
         
         case GET_FULL:
-            console.log([...state.fullOutfits, action.payload]);
-            return { ...state, fullOutfits: [...state.fullOutfits, action.payload]};
+            return { ...state, fullOutfits: action.payload};
 
         default:
             return state;
